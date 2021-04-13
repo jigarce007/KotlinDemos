f00b7237846e51e9ba01ee0d51a5c18fa05f2a7c
jigarce007 <jigarce007@gmail.com>
2021-04-13 12:58:16 +0530

    Initial commit for Data Binding Demo.

---
+++ app/src/main/java/com/ktln/databindingdemo/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.ktln.databindingdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.ktln.databindingdemo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.btnGreet.setOnClickListener{
+            greetMe()
+        }
+    }
+
+
+    private fun greetMe(){
+        binding.apply {
+            tvGreet.text = "Hello  "+etName.text+" ,Welcome to Kotlin."
+        }
+
+
+    }
+}
